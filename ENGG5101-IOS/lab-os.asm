b429bc46b9e0941c1e86208ac7b9cc190d3af032
yzr95924 <741769856@qq.com>
2019-02-13 17:48:56 +0800

    modify the output

--- ENGG5101-IOS/lab-os.asm
+++ ENGG5101-IOS/lab-os.asm
@@ -1,79 +1,79 @@
 ;;; .data
-.word 0                         ; ISR address
+.word 0                         ; ISR address 0
 
 ;;; stack for 2 processes
-.word 0
-.word 0
-.word 0
-.word 0
-.word 0
-.word 0
+.word 0                         ; 1
+.word 0                         ; 2
+.word 0                         ; 3
+.word 0                         ; 4
+.word 0                         ; 5
+.word 0                         ; 6
 A_stack_top:
 
-.word 0
-.word 0
-.word 0
-.word 0
-.word 0
-.word 0
+.word 0                         ; 7
+.word 0                         ; 8
+.word 0                         ; 9
+.word 0                         ; 10
+.word 0                         ; 11
+.word 0                         ; 12
 B_stack_top:
 
 current:
-.word 0                         ; current proc num
+.word 0                         ; current proc num 13 
 process_control_block:
-.word 0                         ; saved sp(A)
-.word 0                         ; saved sp(B)
+.word 0                         ; saved sp(A) 14
+.word 0                         ; saved sp(B) 15
 
 ;;; .text
 OS_start:
     ;; setup ISR
-    movei   R0, 0
-    la      R1, save_context
-    sw      R0, R1, 0
+    movei   R0, 0                 ; 16
+    la      R1, save_context      ; 17
+    sw      R0, R1, 0             ; 18
 
     ;; setup initial context for Process B
-    la      sp, B_stack_top     ; sp <- B_stack_top
+    la      sp, B_stack_top     ; sp <- B_stack_top 19
     ;; save PSR and PC
-    movei   R1, 1               ; bit 1: Interrupt Enabled
-    push    R1
-    la      R1, proc_start      ; initial PC points to process entry point
-    push    R1
+    movei   R1, 1               ; bit 1: Interrupt Enabled 20
+    push    R1                  ; 21
+    la      R1, proc_start      ; initial PC points to process entry point 22
+    push    R1                  ; 23
 
     ;; save(clear) general purpose registers
-    movei   R1, 0
-    push    R1			; Push for R0
-    push    R1			; Push for R1
-    push    R1			; Push for R2
-    movei   R3, 66              ; R3 <- 'B'
-    push    R3                  ; save R3 so that after restoring the context, proc A should see 'A' and proc B should see 'B'
+    movei   R1, 0               ; 24
+    push    R1			; Push for R0   25
+    push    R1			; Push for R1   26
+    push    R1			; Push for R2   27
+    movei   R3, 66              ; R3 <- 'B' 28
+    push    R3                  ; save R3 so that after restoring the context, proc A should see 'A' and proc B should see 'B' 29
 
     ;; save sp 
-    la      R2, process_control_block
-    addi    R2, R2, 1		; sp is saved to process_control_block[1] for context switch 
-    sw      R2, sp, 0
+    la      R2, process_control_block ; 30
+    addi    R2, R2, 1		; sp is saved to process_control_block[1] for context switch 31
+    sw      R2, sp, 0                 ; 32
 
     ;; Execute Process A first
     ;;   First, set up the current process id (0) for Process A 
-    movei   R0, 0		
-    la      R1, current 
-    sw      R1, R0, 0
+    movei   R0, 0		                  ; 33
+    la      R1, current               ; 34
+    sw      R1, R0, 0                 ; 35 
     ;;   Second, set up the inital value of R3 (for print 'A') 
-    movei  R3, 65		; R3 <- 'A'
+    movei  R3, 65		; R3 <- 'A'       ; 36
     ;;   Third, set up sp of Process A
-    la     sp, A_stack_top
+    la     sp, A_stack_top            ; 37
     ;;   Finally, use iret to return back to user
-    movei   R1, 1               ; bit 1: Interrupt Enabled
-    push    R1
-    la      R1, proc_start      ; initial PC points to process entry point
-    push    R1
-    iret
+    movei   R1, 1               ; bit 1: Interrupt Enabled 38
+    push    R1                  ; 39
+    la      R1, proc_start      ; initial PC points to process entry point 40
+    push    R1                  ; 41
+    iret                        ; 42
 
 save_context:
     ;; save general purpose registers
-    push    R0
-    push    R1
-    push    R2
-    push    R3
+    push    R0                  ; 43
+    push    R1                  ; 44 
+    push    R2                  ; 45
+    push    R3                  ; 46
 
     la      R0, current
     lw      R0, R3, 0           ; R3 <- current process id
@@ -103,5 +103,5 @@ switch_proc:                 ; R2 = next process id
     iret
 
 proc_start:                     ; entry point of user process
-    put     R3                  ; print the char in R3 on the screen 
-    jmp     proc_start
+    put     R3                  ; print the char in R3 on the screen  65
+    jmp     proc_start          ; 66
